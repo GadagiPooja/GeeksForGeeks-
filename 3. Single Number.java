3a76aeb58af96b489ed544561217bdb04869c870
Pooja Gadagi <gadagipooja16@gmail.com>
2024-10-18 10:54:25 +0530

    Create 3. Single Number.java

---
+++ 3. Single Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    int getSingle(int arr[]) {
+        // code here
+        int result = 0;
+        for(int num : arr){
+            result ^= num;
+        }
+        return result;
+    }
+}
